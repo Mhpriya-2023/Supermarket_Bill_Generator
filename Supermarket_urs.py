c385510bfe62b615812d2418a381c2d6cd6efd73
Mhpriya-2023 <133908420+Mhpriya-2023@users.noreply.github.com>
2024-03-18 14:32:29 +0530

    Supermarket_urs.py

--- Supermarket_urs.py
+++ Supermarket_urs.py
@@ -1,81 +1,137 @@
 from datetime import datetime
+import mysql.connector
 
-name=input("Enter your name:")
+# Connect to MySQL
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root123",
+    password="Password123#4",
+    database="supermarket"
+)
+mycursor = mydb.cursor()
 
-#list of items
-lists='''
+# Create bills table if it doesn't exist
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS bills (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        total_amount DECIMAL(10, 2),
+        gst_amount DECIMAL(10, 2),
+        final_amount DECIMAL(10, 2),
+        date_time DATETIME
+    )
+""")
+
+# Create bill_items table if it doesn't exist
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS bill_items (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        bill_id INT,
+        item VARCHAR(255),
+        quantity INT,
+        price DECIMAL(10, 2),
+        FOREIGN KEY (bill_id) REFERENCES bills(id)
+    )
+""")
+
+name = input("Enter your name:")
+
+# List of items
+lists = '''
 Rice     rs 20/kg
 Sugar    rs 40/kg
-salt     rs 20/kg
-oil      rs 120/lit
+Salt     rs 20/kg
+Oil      rs 120/lit
 Paneer   rs 150/kg
 Maggie   rs 60/kg
 Horlicks rs 88/kg
 Brush    rs 20/each
 Colgate  rs 38/each
 '''
-price=0
-pricelist=[]
-totalprice=0
-finalprice=0
-ilist=[]
-qlist=[]
-plist=[]
+price = 0
+pricelist = []
+totalprice = 0
+finalprice = 0
+ilist = []
+qlist = []
+plist = []
 
-#rates for items
-items={'Rice':20,
-'Sugar':40,
-'salt':20,
-'oil':120,
-'Paneer':150,
-'Maggie':60,
-'Horlicks':88,
-'Brush':20,'Colgate':38}
+# Rates for items
+items = {
+    'Rice': 20,
+    'Sugar': 40,
+    'Salt': 20,
+    'Oil': 120,
+    'Paneer': 150,
+    'Maggie': 60,
+    'Horlicks': 88,
+    'Brush': 20,
+    'Colgate': 38
+}
 
-option=int(input("for list of items press 1:"))
+option = int(input("for list of items press 1:"))
 
-if option==1:
+if option == 1:
     print(lists)
+
 for i in range(len(items)):
-    inp1=int(input("if you want buy press 1 or 2 for exit:"))
-    if inp1==2:
+    inp1 = int(input("if you want buy press 1 or 2 for exit:"))
+    if inp1 == 2:
         break
-    
-    if inp1==1:
-        item=input("Enter your items:")
-        quantity=int(input("Enter quantity:"))
+
+    if inp1 == 1:
+        item = input("Enter your items:")
+        quantity = int(input("Enter quantity:"))
         if item in items.keys():
-            price=quantity*(items[item])
+            price = quantity * (items[item])
             pricelist.append(price)
-            print(pricelist)
-            totalprice+=price
+            totalprice += price
             ilist.append(item)
             qlist.append(quantity)
-            plist.append(price) 
-            gst=(totalprice*5)/100
-            finalamount=gst+totalprice
+            plist.append(price)
         else:
-            print("sorry you entered item is not available ")  
-            
-print(finalamount)      
-inp=input("can i bill the items yes or no")
-if inp=='yes':
-    pass
-    if finalamount!=0:
-        print(25*"=","Ours supermarket",25*"=")
-        print(28*" ","Urs mart")
-        print("Name:",name,30*" ","Date:",datetime.now())
-        print(75*"-")
-        print("sno",2*" ",'items',4*" ",'quantity',7* " ",'price')
+            print("Sorry, the item you entered is not available.")
+
+# Calculate GST and final amount
+gst = (totalprice * 5) / 100
+finalamount = gst + totalprice
+
+print(finalamount)
+inp = input("Can I bill the items? (yes or no): ")
+if inp.lower() == 'yes':
+    if finalamount != 0:
+        # Insert data into MySQL table
+        sql = "INSERT INTO bills (name, total_amount, gst_amount, final_amount, date_time) VALUES (%s, %s, %s, %s, %s)"
+        val = (name, totalprice, gst, finalamount, datetime.now())
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+        # Fetch the last inserted bill ID
+        bill_id = mycursor.lastrowid
+
+        # Insert individual items into MySQL table
         for i in range(len(pricelist)):
-            print(i,1*" ",5*" ",ilist[i],5*" ",qlist[i],7*" ",plist[i])
-        print(75*"-")
-        print(50*" ",'TotalAmount:','Rs',totalprice)     
-        print("gst amount",50*" ",'Rs',gst)   
-        print(75*"-")  
-        print(50*" ",'finalAmount:','Rs',finalamount) 
-        print(75*"-") 
-print(20*" ","<<<<Thanks for visting>>>>") 
-print(20*" ",".......Visit Again........")
-        
-        
+            sql = "INSERT INTO bill_items (bill_id, item, quantity, price) VALUES (%s, %s, %s, %s)"
+            val = (bill_id, ilist[i], qlist[i], plist[i])
+            mycursor.execute(sql, val)
+            mydb.commit()
+
+        # Print the bill
+        print(25 * "=", "Ours Supermarket", 25 * "=")
+        print(28 * " ", "Urs Mart")
+        print("Name:", name, 30 * " ", "Date:", datetime.now())
+        print(75 * "-")
+        print("Sno", 2 * " ", 'Items', 4 * " ", 'Quantity', 7 * " ", 'Price')
+        for i in range(len(pricelist)):
+            print(i + 1, 1 * " ", 5 * " ", ilist[i], 5 * " ", qlist[i], 7 * " ", plist[i])
+        print(75 * "-")
+        print(50 * " ", 'Total Amount:', 'Rs', totalprice)
+        print("GST Amount", 50 * " ", 'Rs', gst)
+        print(75 * "-")
+        print(50 * " ", 'Final Amount:', 'Rs', finalamount)
+        print(75 * "-")
+        print(20 * " ", "<<<<Thanks for visiting>>>>")
+        print(20 * " ", ".......Visit Again........")
+
+# Close MySQL connection
+mydb.close()
